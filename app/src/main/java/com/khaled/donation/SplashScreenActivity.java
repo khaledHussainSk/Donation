f5135bc621b36efdd14b5640e797dd849870be74
AhmedHaEL <99762827+AhmedHaEL@users.noreply.github.com>
2022-04-14 18:34:36 +0300

    Search and Notifications

--- app/src/main/java/com/khaled/donation/SplashScreenActivity.java
+++ app/src/main/java/com/khaled/donation/SplashScreenActivity.java
@@ -10,15 +10,10 @@ import android.widget.Toast;
 import com.khaled.donation.databinding.ActivitySplashScreenBinding;
 
 public class SplashScreenActivity extends AppCompatActivity {
-//    ActivitySplashScreenBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-//            binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
-        }catch (Exception e){
-
-        }
         setContentView(R.layout.activity_splash_screen);
 
         new Thread(new Runnable() {
